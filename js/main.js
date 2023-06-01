209ef31808966f2e67693329c1a03a2108486694
SaraOsacar <saraosacar@gmail.com>
2023-06-01 07:56:57 +0200

    add index

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const button = document.querySelector('.js_button');
+const quantity = document.querySelector('.js_quantity');
+
+
+
+
+
+
+button.addEventListener('click', handleClickButton);
+
+
+
