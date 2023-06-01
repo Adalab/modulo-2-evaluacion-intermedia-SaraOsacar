80e5c3eff38776bd0122388084c0145e920a0b53
SaraOsacar <saraosacar@gmail.com>
2023-06-01 20:28:22 +0200

    add js structure

--- js/main.js
+++ js/main.js
@@ -2,12 +2,35 @@
 
 const button = document.querySelector('.js_button');
 const quantity = document.querySelector('.js_quantity');
+const userNumberSelect = document.querySelector('.js_usernumber');
+const result = document.querySelector('.js_result');
+
+
+let amount = 50; 
+
+function getRandomNumber(max) { 
+  return Math.ceil(Math.random() * max);
+}
+
+
+
+const userNumber = userNumberSelect.Value;
+console.log(userNumber);
 
 
 
 
 
 
+//FUNCIONES
+
+const handleClickButton = (event) => {
+    event.preventDefault()
+}
+
+
+
+//EVENTO
 button.addEventListener('click', handleClickButton);
 
 
